4edc835d008f14553e66d2b0b979f42b9a74adf2
Matthew Franco <mattfranco22@gmail.com>
2022-12-05 23:17:18 -0800

    Added Service Client

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,8 @@
-// using UberClient.HTTPClient;
 using UberClient.Services;
 using UberClient.HTTPClient;
+using Microsoft.EntityFrameworkCore.Internal;
+using InternalAPI;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,9 +25,40 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-// HttpClientInstance.InitializeClient();
+HttpClientInstance.InitializeClient();
 
 app.MapGrpcService<EstimatesService>();
 app.MapGrpcService<RequestsService>();
+
+var servicesClient = new Services.ServicesClient(GrpcChannel.ForAddress($"services.api"));
+var request = new RegisterServiceRequest
+{
+    Id = "d4abaae7-f4d6-4152-91cc-77523e8165a4",
+    Name = "uber BLACK",
+    ClientName = "uber",
+};
+request.Features.Add(ServiceFeatures.ProfessionalDriver);
+servicesClient.RegisterService(request);
+request.Features.Clear();
+
+var request = new RegisterServiceRequest
+{
+    Id = "26546650-e557-4a7b-86e7-6a3942445247",
+    Name = "uber POOL",
+    ClientName = "uber",
+};
+request.Features.Add(ServiceFeatures.Shared);
+servicesClient.RegisterService(request);
+request.Features.Clear();
+
+var request = new RegisterServiceRequest
+{
+    Id = "2d1d002b-d4d0-4411-98e1-673b244878b2",
+    Name = "uber X",
+    ClientName = "uber",
+};
+request.Features.Add(ServiceFeatures.ProfessionalDriver);
+servicesClient.RegisterService(request);
+request.Features.Clear();
 
 app.Run();
