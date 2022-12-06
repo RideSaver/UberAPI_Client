195fb3acb9454057196c2134f1ef88c929bc1fb2
Matthew Franco <mattfranco22@gmail.com>
2022-12-05 23:16:29 -0800

    Added EstimateInfo.cs

--- Server/Models/EstimateCache.cs
+++ Server/Models/EstimateCache.cs
@@ -1,10 +1,11 @@
 using UberAPI.Client.Model;
 using InternalAPI;
+using UberClient.Models;
 namespace UberClient.Models
 {
     public class EstimateCache
     {
-        public PriceEstimate PriceEstimate{ get; set; }
+        public EstimateInfo EstimateInfo { get; set; }
         public GetEstimatesRequest GetEstimatesRequest { get; set; }
         public Guid ProductId { get; set; }
         public CurrencyModel CancellationCost { get; set; }
