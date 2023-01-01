c43024ab1574012488c3710e24da00e8de65487c
Elias Schablowski <eschablowski@me.com>
2022-12-31 23:16:47 -0800

    Add logging to Service Registration
    Signed-off-by: Elias Schablowski <eschablowski@me.com>

--- Server/Services/ServicesService.cs
+++ Server/Services/ServicesService.cs
@@ -6,22 +6,26 @@ namespace Services.ServicesService
     public class ServicesService : InternalAPI.Services.ServicesClient
     {
         private readonly InternalAPI.Services.ServicesClient _services;
+        private readonly ILogger<ServicesService> _logger;
 
-        public ServicesService(InternalAPI.Services.ServicesClient services)
+        public ServicesService(InternalAPI.Services.ServicesClient services, ILogger<ServicesService> logger)
         {
             _services = services;
+            _logger = logger;
             RegisterServiceRequest();
         }
 
         public void RegisterServiceRequest()
         {
+            _logger.LogInformation("Registering Services");
             var request = new RegisterServiceRequest
             {
                 Id = ByteString.CopyFrom(Guid.Parse("d4abaae7-f4d6-4152-91cc-77523e8165a4").ToByteArray()),
                 Name = "UberBLACK",
-                ClientName = "Uber",
+                ClientName = "uber",
             };
             request.Features.Add(ServiceFeatures.ProfessionalDriver);
+            _logger.LogDebug("Registering UberBLACK");
             _services.RegisterService(request);
             request.Features.Clear();
 
@@ -29,9 +33,10 @@ namespace Services.ServicesService
             {
                 Id = ByteString.CopyFrom(Guid.Parse("26546650-e557-4a7b-86e7-6a3942445247").ToByteArray()),
                 Name = "UberPOOL",
-                ClientName = "Uber",
+                ClientName = "uber",
             };
             request.Features.Add(ServiceFeatures.Shared);
+            _logger.LogDebug("Registering UberPOOL");
             _services.RegisterService(request);
             request.Features.Clear();
 
@@ -39,9 +44,10 @@ namespace Services.ServicesService
             {
                 Id = ByteString.CopyFrom(Guid.Parse("2d1d002b-d4d0-4411-98e1-673b244878b2").ToByteArray()),
                 Name = "UberX",
-                ClientName = "Uber",
+                ClientName = "uber",
             };
             request.Features.Add(ServiceFeatures.ProfessionalDriver);
+            _logger.LogDebug("Registering UberX");
             _services.RegisterService(request);
             request.Features.Clear();
         }
