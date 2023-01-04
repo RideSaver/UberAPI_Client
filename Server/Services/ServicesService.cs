676ad80440e92e27349c9f33af9937fa21ba6560
Elias Schablowski <eschablowski@me.com>
2023-01-03 17:02:22 -0800

    Extract service registration to function
    Signed-off-by: Elias Schablowski <eschablowski@me.com>

--- Server/Services/ServicesService.cs
+++ Server/Services/ServicesService.cs
@@ -16,42 +16,39 @@ namespace Services.ServicesService
             _logger = logger;
         }
 
+        private async Task RegisterService(string id, string name, IEnumerable<ServiceFeatures> features)
+        {
+            var request = new RegisterServiceRequest
+            {
+                Id = ByteString.CopyFrom(Guid.Parse(id).ToByteArray()),
+                Name = name,
+                ClientName = "Uber",
+            };
+
+            foreach (var feature in features)
+            {
+                request.Features.Add(ServiceFeatures.ProfessionalDriver);
+            }
+
+            _logger.LogDebug($"[UberClient::ServicesService::RegisterServiceRequest] Registering [{name}] service...");
+            await _services.RegisterServiceAsync(request);
+        }
+
         public async Task RegisterServiceRequest()
         {
             _logger.LogInformation("[UberClient::ServicesService::RegisterServiceRequest] Registering services...");
 
-            var requestUB = new RegisterServiceRequest
-            {
-                Id = ByteString.CopyFrom(Guid.Parse("d4abaae7-f4d6-4152-91cc-77523e8165a4").ToByteArray()),
-                Name = "UberBLACK",
-                ClientName = "Uber",
-            };
+            RegisterService("d4abaae7-f4d6-4152-91cc-77523e8165a4", "UberBlack", new ({
+                ServiceFeatures.ProfessionalDriver
+            }));
 
-            requestUB.Features.Add(ServiceFeatures.ProfessionalDriver);
-            _logger.LogDebug("[UberClient::ServicesService::RegisterServiceRequest] Registering [UberBLACK] service...");
-            var replyUB = await _services.RegisterServiceAsync(requestUB);
+            RegisterService("26546650-e557-4a7b-86e7-6a3942445247", "UberPOOL", new ({
+                ServiceFeatures.Shared
+            }));
 
-            var requestUP = new RegisterServiceRequest
-            {
-                Id = ByteString.CopyFrom(Guid.Parse("26546650-e557-4a7b-86e7-6a3942445247").ToByteArray()),
-                Name = "UberPOOL",
-                ClientName = "Uber",
-            };
-
-            requestUP.Features.Add(ServiceFeatures.Shared);
-            _logger.LogDebug("[UberClient::ServicesService::RegisterServiceRequest] Registering [UberPOOL] service...");
-            var replyUP = await _services.RegisterServiceAsync(requestUP);
-
-            var requestUX = new RegisterServiceRequest
-            {
-                Id = ByteString.CopyFrom(Guid.Parse("2d1d002b-d4d0-4411-98e1-673b244878b2").ToByteArray()),
-                Name = "UberX",
-                ClientName = "Uber",
-            };
-
-            requestUX.Features.Add(ServiceFeatures.ProfessionalDriver);
-            _logger.LogDebug("[UberClient::ServicesService::RegisterServiceRequest] Registering [UberX] service...");
-            var replyUX = await _services.RegisterServiceAsync(requestUX);
+            RegisterService("2d1d002b-d4d0-4411-98e1-673b244878b2", "UberX", new ({
+                ServiceFeatures.ProfessionalDriver
+            }));
 
             _logger.LogInformation("[UberClient::ServicesService::RegisterServiceRequest] Services Registeration complete.");
         }
