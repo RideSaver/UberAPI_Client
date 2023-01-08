55614b10b1750d6e171c4290a7ac2a30c9a62125
JPychon <64781464+JPychon@users.noreply.github.com>
2023-01-08 04:17:54 -0800

    Syntax tweaks.

--- Server/Extensions/ServiceLinker.cs
+++ Server/Extensions/ServiceLinker.cs
@@ -1,6 +1,13 @@
-﻿namespace UberClient.Extensions
+namespace UberClient.Extensions
 {
     public class ServiceLinker
     {
+        public static readonly Dictionary<string, string> ServiceIDs = new Dictionary<string, string>
+        {
+            {"d4abaae7-f4d6-4152-91cc-77523e8165a4", "UberBLACK"},
+            {"26546650-e557-4a7b-86e7-6a3942445247", "UberPOOL"},
+            {"2d1d002b-d4d0-4411-98e1-673b244878b2", "UberX"},
+        };
     }
+
 }
